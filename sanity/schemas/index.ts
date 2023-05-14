6dd4662061f3ad8a78d29eef9526372d0a1e0c2f
kedat <Datmartin10@gmail.com>
2023-05-14 13:30:35 +0700

    Feat - Add hamburger table

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -5,4 +5,5 @@ import users from './user'
 import admin from './admin'
 import faq from './faq'
 import reviewer from './reviewer'
-export const schemaTypes = [pizza, order, category, users, admin, faq, reviewer]
+import hamburger from './hamburger'
+export const schemaTypes = [pizza, order, category, users, admin, faq, reviewer, hamburger]
