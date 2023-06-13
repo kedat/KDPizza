7dac8887b7d6f2c1c4a07dc3aea748168699fd6b
kedat <datmartin10@gmail.com>
2023-06-13 09:04:27 +0700

    Feat - Update order in profile

--- components/Profile/Order.jsx
+++ components/Profile/Order.jsx
@@ -33,7 +33,7 @@ const AllOrder = ({ userOrders }) => {
                   {order.status === 2 && <td className='border px-8 py-4'>On way</td>}
                   {order.status === 3 && <td className='border px-8 py-4'>Delivered</td>}
                   {order.status === 4 && <td className='border px-8 py-4'>Completed</td>}
-                  {order.status === 5 && <td className='border px-8 py-4'>Canceled</td>}
+                  {order.status === -1 && <td className='border px-8 py-4'>Canceled</td>}
                   <td className='border px-8 py-4'>{order.method === 0 ? 'Pay on Delivery' : 'Online Payment'}</td>
                   <td className='border px-8 py-4'>{order._createdAt}</td>
                   <td className='border px-8 py-4'>{order._updatedAt}</td>
